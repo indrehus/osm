fe01d700d652e6b1b9ff788d401df9216b8ee272
Nichodemius <nichodemius@hotmail.com>
2013-03-11 10:51:13 +0100

    Fixed lock_cond.c for resub.

--- kernel/lock_cond.c
+++ kernel/lock_cond.c
@@ -2,35 +2,51 @@
 #include "kernel/sleepq.h"
 #include "kernel/thread.h"
 #include "kernel/interrupt.h"
+#include "kernel/spinlock.h"
 
 #define LOCK_RESET_SUCCESS 0
 #define LOCK_RESET_FAILURE -1
+#define LOCK_OPEN 0
+#define LOCK_CLOSED 1
 
 int lock_reset (lock_t *lock)
 {
-  if (*lock == 0) {
+  interrupt_status_t state = _interrupt_disable();
+  spinlock_acquire(lock);
+  if (*lock == LOCK_CLOSED) {
+    spinlock_release(lock);
+    _interrupt_set_state(state);
     return LOCK_RESET_FAILURE;
   }
-  *lock = 1;
+  *lock = LOCK_OPEN;    
+  spinlock_release(lock);
+  _interrupt_set_state(state);
   return LOCK_RESET_SUCCESS;
 }
 
 void lock_acquire(lock_t *lock)
 {
   interrupt_status_t state = _interrupt_disable();
-  while (*lock != 1) {
+  spinlock_acquire(lock);
+  /* While lock is closed, since LOCK_CLOSED == 1 */
+  while (*lock) {
     sleepq_add(lock);
+    spinlock_release(lock);
     thread_switch();
+    spinlock_acquire(lock);
   }
-  *lock = 0;
+  *lock = LOCK_CLOSED;
+  spinlock_release(lock);
   _interrupt_set_state(state);
 }
 
 void lock_release(lock_t *lock)
 {
   interrupt_status_t state = _interrupt_disable();
-  *lock = 1;
+  spinlock_acquire(lock);
+  *lock = LOCK_OPEN;
   sleepq_wake(lock);
+  spinlock_release(lock);
   _interrupt_set_state(state);
 }
 
@@ -49,14 +65,12 @@ void condition_wait(cond_t *cond, lock_t *lock)
 
 void condition_signal(cond_t *cond, lock_t *lock)
 {
-  lock_acquire(lock);
+  lock = lock;
   sleepq_wake(cond);
-  lock_release(lock);
 }
 
 void condition_broadcast(cond_t *cond, lock_t *lock)
 {
-  lock_acquire(lock);
+  lock = lock;
   sleepq_wake_all(cond);
-  lock_release(lock);
 }
